d26bf28103dfc3ea71f6f931912c2d85fc5b7cab
ZaidShaikh <szaid5146@gmail.com>
2025-03-13 22:47:31 +0530

    Added new text field

--- currency_convertor_app/lib/currenct_convertor_materialpage.dart
+++ currency_convertor_app/lib/currenct_convertor_materialpage.dart
@@ -10,8 +10,29 @@ class CurrencyConvertor extends StatelessWidget {
         centerTitle: true,
         title: Text("Currency Convertor"),
       ),
-      body: Column(
-        children: [Text("data")],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "0",
+              style: TextStyle(
+                  fontSize: 55,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueGrey),
+            ),
+            TextField(
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                  label: Text(
+                "Please enter the amount in INR: ",
+                style: TextStyle(color: Colors.amber),
+              )),
+            )
+          ],
+        ),
       ),
     );
   }
